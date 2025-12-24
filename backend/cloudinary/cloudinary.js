23debc62ccb372f73bbd3cdf592ad826d3da8942
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-12-24 22:07:53 +0530

    update

--- backend/cloudinary/cloudinary.js
+++ backend/cloudinary/cloudinary.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME||dgp9pbjqs,
+  api_key: process.env.CLOUDINARY_API_KEY||445678712767251,
+  api_secret: process.env.CLOUDINARY_API_SECRET||ES7o0_Mv0QYdlcveAtOTyxCHOis,
 });
 
 export default cloudinary;
