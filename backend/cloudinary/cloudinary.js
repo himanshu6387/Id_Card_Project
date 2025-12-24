36bd58eb0e2565760e7b5febe0536803f7f3b04e
himanshu6387 <mishrahimanshuu01@gmail.com>
2025-12-24 21:44:08 +0530

    update

--- backend/cloudinary/cloudinary.js
+++ backend/cloudinary/cloudinary.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 cloudinary.v2.config({
   // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  cloud_name: dingg2tq9,
+  cloud_name:dingg2tq9,
   // api_key: process.env.CLOUDINARY_API_KEY,
-  api_key: 976634486382574,
+  api_key:976634486382574,
   // api_secret: process.env.CLOUDINARY_API_SECRET,
   api_secret:BEQ3q2x3R8fqUPaRDhpsnDUW11k,
 });
