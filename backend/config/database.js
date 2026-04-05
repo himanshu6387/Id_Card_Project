8fae3d5e5cba5b96a9a35acaaba599aada496689
himanshu6387 <mishrahimanshuu01@gmail.com>
2026-04-05 16:17:04 +0530

    save

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,8 @@
     const connectDB = async () => {
         try {
             // mongoose.connect('mongodb://himanshu_db_user:Himanshu123@cluster0.wphdsts.mongodb.net/idcardDB?retryWrites=true&w=majority&appName=Cluster0')
-            mongoose.connect('mongodb://himanshu_db_user:LXjlMT0cOTFVb1l7@cluster0.wphdsts.mongodb.net/?appName=Cluster0')
+            // mongoose.connect('mongodb://himanshu_db_user:LXjlMT0cOTFVb1l7@cluster0.wphdsts.mongodb.net/?appName=Cluster0')
+            mongoose.connect('mongodb://ravitejasmartbridge_db_user:q7NozX2FeDDyYibp@ac-qi77l0p-shard-00-00.lxhpbiz.mongodb.net:27017,ac-qi77l0p-shard-00-01.lxhpbiz.mongodb.net:27017,ac-qi77l0p-shard-00-02.lxhpbiz.mongodb.net:27017/?ssl=true&replicaSet=atlas-11z8tu-shard-0&authSource=admin&appName=IdCardProject')
             // mongoose.connect('mongodb://localhost:27017/id_Card')
             console.log('MongoDB Connected Successfully..')
         } catch (error) {
